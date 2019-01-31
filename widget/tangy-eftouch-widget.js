e427fe45a4c33b87a52168564bea56980d5dd6aa
Evans D <evansdianga@gmail.com>
2019-01-31 12:11:54 +0300

    Added print output to widgets

--- widget/tangy-eftouch-widget.js
+++ widget/tangy-eftouch-widget.js
@@ -1,15 +1,14 @@
-import '@polymer/paper-card/paper-card.js'
-import '@polymer/paper-button/paper-button.js'
-import 'tangy-form/tangy-form.js'
-import 'tangy-form/input/tangy-eftouch.js'
-import 'tangy-form/input/tangy-input.js'
-import './tangy-eftouch-widget-layout.js'
-import { TangyBaseWidget } from '../tangy-base-widget.js'
+import '@polymer/paper-card/paper-card.js';
+import '@polymer/paper-button/paper-button.js';
+import 'tangy-form/tangy-form.js';
+import 'tangy-form/input/tangy-eftouch.js';
+import 'tangy-form/input/tangy-input.js';
+import './tangy-eftouch-widget-layout.js';
+import { TangyBaseWidget } from '../tangy-base-widget.js';
 
 class TangyEftouchWidget extends TangyBaseWidget {
-
   get claimElement() {
-    return 'tangy-eftouch'
+    return 'tangy-eftouch';
   }
 
   get defaultConfig() {
@@ -27,16 +26,16 @@ class TangyEftouchWidget extends TangyBaseWidget {
       autoProgress: false,
       required: false,
       disabled: false,
-      hidden: false,
-    }
+      hidden: false
+    };
   }
 
   upcast(config, element) {
     return {
-      ...config, 
+      ...config,
       ...element.getProps(),
       optionsMarkup: element.innerHTML
-    }
+    };
   }
 
   downcast(config) {
@@ -58,12 +57,14 @@ class TangyEftouchWidget extends TangyBaseWidget {
       >
         ${config.optionsMarkup}
       </tangy-eftouch>
-    `
+    `;
   }
-  
+
   renderInfo(config) {
-    return `<div class="element-header"><div><mwc-icon>question_answer</mwc-icon></div><div id="element-name">${config.name}</div></div>
-    ${this.downcast(config)}`
+    return `<div class="element-header"><div><mwc-icon>question_answer</mwc-icon></div><div id="element-name">${
+      config.name
+    }</div></div>
+    ${this.downcast(config)}`;
   }
 
   renderEdit(config) {
@@ -71,28 +72,91 @@ class TangyEftouchWidget extends TangyBaseWidget {
     <tangy-form id="tangy-eftouch">
       <tangy-form-item id="tangy-eftouch">
         <template type="tangy-form-item">
-          <tangy-input name="name" label="Variable name" value="${config.name}" required></tangy-input>
-          <tangy-input name="label" label="Label" value="${config.label}"></tangy-input>
-          <tangy-input name="input-sound" label="Input sound" value="${config.inputSound}"></tangy-input>
-          <tangy-input name="transition-sound" label="Transition sound" value="${config.transitionSound}"></tangy-input>
-          <tangy-input name="transition-delay" label="Transition delay" value="${config.transitionDelay}" type="number"></tangy-input>
-          <tangy-input name="transition-message" label="Transition message" value="${config.transitionMessage}"></tangy-input>
-          <tangy-input name="warning-time" label="Warning time" value="${config.warningTime}"></tangy-input>
-          <tangy-input name="warning-message" label="Warning message" value="${config.warningMessage}"></tangy-input>
-          <tangy-input name="time-limit" label="Time limit" value="${config.timeLimit}"></tangy-input>
-          <tangy-checkbox name="auto-progress" ${config.autoProgress ? 'value="on"' : ''}>auto-progress</tangy-checkbox>
-          <tangy-checkbox name="required" ${config.required ? 'value="on"' : ''}>Required</tangy-checkbox>
-          <tangy-checkbox name="disabled" ${config.disabled ? 'value="on"' : ''}>Disabled</tangy-checkbox>
-          <tangy-checkbox name="hidden" ${config.hidden ? 'value="on"' : ''}>Hidden</tangy-checkbox>
+          <tangy-input name="name" label="Variable name" value="${
+            config.name
+          }" required></tangy-input>
+          <tangy-input name="label" label="Label" value="${
+            config.label
+          }"></tangy-input>
+          <tangy-input name="input-sound" label="Input sound" value="${
+            config.inputSound
+          }"></tangy-input>
+          <tangy-input name="transition-sound" label="Transition sound" value="${
+            config.transitionSound
+          }"></tangy-input>
+          <tangy-input name="transition-delay" label="Transition delay" value="${
+            config.transitionDelay
+          }" type="number"></tangy-input>
+          <tangy-input name="transition-message" label="Transition message" value="${
+            config.transitionMessage
+          }"></tangy-input>
+          <tangy-input name="warning-time" label="Warning time" value="${
+            config.warningTime
+          }"></tangy-input>
+          <tangy-input name="warning-message" label="Warning message" value="${
+            config.warningMessage
+          }"></tangy-input>
+          <tangy-input name="time-limit" label="Time limit" value="${
+            config.timeLimit
+          }"></tangy-input>
+          <tangy-checkbox name="auto-progress" ${
+            config.autoProgress ? 'value="on"' : ''
+          }>auto-progress</tangy-checkbox>
+          <tangy-checkbox name="required" ${
+            config.required ? 'value="on"' : ''
+          }>Required</tangy-checkbox>
+          <tangy-checkbox name="disabled" ${
+            config.disabled ? 'value="on"' : ''
+          }>Disabled</tangy-checkbox>
+          <tangy-checkbox name="hidden" ${
+            config.hidden ? 'value="on"' : ''
+          }>Hidden</tangy-checkbox>
           <tangy-eftouch-widget-layout name="options-markup">
             ${config.optionsMarkup}
           </tangy-eftouch-widget-layout>
         </template>
       </tangy-form-item>
     </tangy-form>
-    `
+    `;
   }
 
+  renderPrint(config) {
+    return `
+   
+    <table>
+    <tr><td><strong>Variable Name:</strong></td><td>${config.name}</td></tr>
+    <tr><td><strong>Variable Label:</strong></td><td>${config.label}</td></tr>
+    <tr><td><strong>Hint:</strong></td><td>${config.hint}</td></tr>
+    <tr><td><strong>Input Sound:</strong></td><td>${config.inputSound}</td></tr>
+    <tr><td><strong>Transition Sound:</strong></td><td>${
+      config.transitionSound
+    }</td></tr>
+      <tr><td><strong>Transition Delay:</strong></td><td>${
+        config.transitionDelay
+      }</td></tr>
+      <tr><td><strong>Transition Message:</strong></td><td>${
+        config.transitionMessage
+      }</td></tr>
+      <tr><td><strong>Warning Time:</strong></td><td>${
+        config.warningTime
+      }</td></tr>
+      <tr><td><strong>Warning Message:</strong></td><td>${
+        config.warningMessage
+      }</td></tr>
+      <tr><td><strong>Time Limit:</strong></td><td>${config.timeLimit}</td></tr>
+      <tr><td><strong>Auto Progress:</strong></td><td>${
+        config.autoProgress
+      }</td></tr>
+      <tr><td><strong>Required:</strong></td><td>${config.required}</td></tr>
+      <tr><td><strong>Disabled:</strong></td><td>${config.disabled}</td></tr>
+      <tr><td><strong>Hidden:</strong></td><td>${config.hidden}</td></tr>
+      <tr><td><strong>Options Markup:</strong></td><td><ul>${
+        config.optionsMarkup
+      }</ul></td></tr>
+    </table>
+    <hr/>
+    `;
+  }
   onSubmit(config, formEl) {
     return {
       ...config,
@@ -110,10 +174,13 @@ class TangyEftouchWidget extends TangyBaseWidget {
       hidden: formEl.values.hidden === 'on' ? true : false,
       disabled: formEl.values.disabled === 'on' ? true : false,
       optionsMarkup: formEl.values['options-markup']
-    }
+    };
   }
-
 }
 
 window.customElements.define('tangy-eftouch-widget', TangyEftouchWidget);
-window.tangyFormEditorWidgets.define('tangy-eftouch-widget', 'tangy-eftouch', TangyEftouchWidget);
+window.tangyFormEditorWidgets.define(
+  'tangy-eftouch-widget',
+  'tangy-eftouch',
+  TangyEftouchWidget
+);
--- widget/tangy-location-widget.js
+++ widget/tangy-location-widget.js
@@ -1,13 +1,12 @@
-import '@polymer/paper-card/paper-card.js'
-import '@polymer/paper-button/paper-button.js'
-import 'tangy-form/input/tangy-location.js'
-import 'tangy-form/input/tangy-checkbox.js'
-import { TangyBaseWidget } from '../tangy-base-widget.js'
+import '@polymer/paper-card/paper-card.js';
+import '@polymer/paper-button/paper-button.js';
+import 'tangy-form/input/tangy-location.js';
+import 'tangy-form/input/tangy-checkbox.js';
+import { TangyBaseWidget } from '../tangy-base-widget.js';
 
 class TangyLocationWidget extends TangyBaseWidget {
-
   get claimElement() {
-    return 'tangy-location'
+    return 'tangy-location';
   }
 
   get defaultConfig() {
@@ -21,24 +20,28 @@ class TangyLocationWidget extends TangyBaseWidget {
       metaDataTemplate: '',
       filterByGlobal: false,
       showLevels: ''
-    }
+    };
   }
 
   upcast(config, element) {
     // @TODO We have to do that final thing for tangyIf because it's not declared a prop in TangyLocation.props thus won't get picked up by TangyLocation.getProps().
-    return {...config, ...element.getProps(), ...{
-      tangyIf: element.hasAttribute('tangy-if')
-        ? element.getAttribute('tangy-if')
-        : '',
-      metaDataTemplate: element.innerHTML
-    }}
+    return {
+      ...config,
+      ...element.getProps(),
+      ...{
+        tangyIf: element.hasAttribute('tangy-if')
+          ? element.getAttribute('tangy-if')
+          : '',
+        metaDataTemplate: element.innerHTML
+      }
+    };
   }
 
   downcast(config) {
     return `
       <tangy-location 
         name="${config.name}"
-        ${config.tangyIf === "" ? "" : `tangy-if="${config.tangyIf}"`}
+        ${config.tangyIf === '' ? '' : `tangy-if="${config.tangyIf}"`}
         ${config.required ? 'required' : ''}
         ${config.disabled ? 'disabled' : ''}
         ${config.hidden ? 'hidden' : ''}
@@ -48,48 +51,112 @@ class TangyLocationWidget extends TangyBaseWidget {
       >
         ${config.metaDataTemplate}
       </tangy-location>
-    `
+    `;
   }
-  
+  renderPrint(config) {
+    return `
+
+    <table>
+      <tr><td><strong>Location Levels:</strong></td><td>${
+        config.showLevels
+      }</td></tr>
+      <tr><td><strong>Variable Name:</strong></td><td>${config.name}</td></tr>
+      <tr><td><strong>Hint:</strong></td><td>${config.hint}</td></tr>
+      <tr><td><strong>Required:</strong></td><td>${config.required}</td></tr>
+      <tr><td><strong>Disabled:</strong></td><td>${config.disabled}</td></tr>
+      <tr><td><strong>Hidden:</strong></td><td>${config.hidden}</td></tr>
+    </table>
+    <hr/>
+    `;
+  }
   renderInfo(config) {
-    return `<div class="element-header"><div><mwc-icon>location_city</mwc-icon></div><div id="element-name">${config.name}</div></div>
-    ${this.downcast(config)}`
+    return `<div class="element-header"><div><mwc-icon>location_city</mwc-icon></div><div id="element-name">${
+      config.name
+    }</div></div>
+    ${this.downcast(config)}`;
   }
 
   renderEdit(config) {
     return `<h2>Add Location Element</h2>
     <tangy-form id="tangy-location">
       <tangy-form-item>
-        <tangy-input name="name" label="Variable name" value="${config.name}" required></tangy-input>
-        <tangy-checkbox name="filterByGlobal" ${config.filterByGlobal ? 'value="on"' : ''}>Filter by locations in the user profile?</tangy-checkbox>
-        <tangy-input name="showLevels" label="Show levels (ex. county,subcounty)" value="${config.showLevels}"></tangy-input>
-        <tangy-input name="tangy_if" label="Show if" value="${config.tangyIf}"></tangy-input>
-        <tangy-checkbox name="required" ${config.required ? 'value="on"' : ''}>Required</tangy-checkbox>
-        <tangy-checkbox name="disabled" ${config.disabled ? 'value="on"' : ''}>Disabled</tangy-checkbox>
-        <tangy-checkbox name="hidden" ${config.hidden ? 'value="on"' : ''}>Hidden</tangy-checkbox>
-        <tangy-checkbox name="show-meta-data" ${config.showMetaData ? 'value="on"' : ''}>show meta-data</tangy-checkbox>
-        <tangy-input name="meta-data-template" label="Meta-data template" value="${config.metaDataTemplate}"></tangy-input>
+        <tangy-input name="name" label="Variable name" value="${
+          config.name
+        }" required></tangy-input>
+        <tangy-checkbox name="filterByGlobal" ${
+          config.filterByGlobal ? 'value="on"' : ''
+        }>Filter by locations in the user profile?</tangy-checkbox>
+        <tangy-input name="showLevels" label="Show levels (ex. county,subcounty)" value="${
+          config.showLevels
+        }"></tangy-input>
+        <tangy-input name="tangy_if" label="Show if" value="${
+          config.tangyIf
+        }"></tangy-input>
+        <tangy-checkbox name="required" ${
+          config.required ? 'value="on"' : ''
+        }>Required</tangy-checkbox>
+        <tangy-checkbox name="disabled" ${
+          config.disabled ? 'value="on"' : ''
+        }>Disabled</tangy-checkbox>
+        <tangy-checkbox name="hidden" ${
+          config.hidden ? 'value="on"' : ''
+        }>Hidden</tangy-checkbox>
+        <tangy-checkbox name="show-meta-data" ${
+          config.showMetaData ? 'value="on"' : ''
+        }>show meta-data</tangy-checkbox>
+        <tangy-input name="meta-data-template" label="Meta-data template" value="${
+          config.metaDataTemplate
+        }"></tangy-input>
       </tangy-form-item>
     </tangy-form>
-    `
+    `;
   }
 
   onSubmit(config, formEl) {
     return {
       ...config,
-      name: formEl.response.items[0].inputs.find(input => input.name === 'name').value,
-      required: formEl.response.items[0].inputs.find(input => input.name === 'required').value === 'on' ? true : false,
-      hidden: formEl.response.items[0].inputs.find(input => input.name === 'hidden').value === 'on' ? true : false,
-      disabled: formEl.response.items[0].inputs.find(input => input.name === 'disabled').value === 'on' ? true : false,
-      tangyIf: formEl.response.items[0].inputs.find(input => input.name === 'tangy_if').value,
-      filterByGlobal: formEl.response.items[0].inputs.find(input => input.name === 'filterByGlobal').value,
-      showLevels: formEl.response.items[0].inputs.find(input => input.name === 'showLevels').value,
-      showMetaData: formEl.response.items[0].inputs.find(input => input.name === 'show-meta-data').value === 'on' ? true : false,
-      metaDataTemplate: formEl.response.items[0].inputs.find(input => input.name === 'meta-data-template').value,
-    }
+      name: formEl.response.items[0].inputs.find(input => input.name === 'name')
+        .value,
+      required:
+        formEl.response.items[0].inputs.find(input => input.name === 'required')
+          .value === 'on'
+          ? true
+          : false,
+      hidden:
+        formEl.response.items[0].inputs.find(input => input.name === 'hidden')
+          .value === 'on'
+          ? true
+          : false,
+      disabled:
+        formEl.response.items[0].inputs.find(input => input.name === 'disabled')
+          .value === 'on'
+          ? true
+          : false,
+      tangyIf: formEl.response.items[0].inputs.find(
+        input => input.name === 'tangy_if'
+      ).value,
+      filterByGlobal: formEl.response.items[0].inputs.find(
+        input => input.name === 'filterByGlobal'
+      ).value,
+      showLevels: formEl.response.items[0].inputs.find(
+        input => input.name === 'showLevels'
+      ).value,
+      showMetaData:
+        formEl.response.items[0].inputs.find(
+          input => input.name === 'show-meta-data'
+        ).value === 'on'
+          ? true
+          : false,
+      metaDataTemplate: formEl.response.items[0].inputs.find(
+        input => input.name === 'meta-data-template'
+      ).value
+    };
   }
-
 }
 
 window.customElements.define('tangy-location-widget', TangyLocationWidget);
-window.tangyFormEditorWidgets.define('tangy-location-widget', 'tangy-location', TangyLocationWidget);
+window.tangyFormEditorWidgets.define(
+  'tangy-location-widget',
+  'tangy-location',
+  TangyLocationWidget
+);
