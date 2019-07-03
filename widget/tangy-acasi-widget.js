32564f5fc3178f853145c9038813fc9f9487e956
Chris E. Kelley <ckelley@rti.org>
2019-07-03 11:13:46 +0200

    Work on getting changed attributes to save

--- widget/tangy-acasi-widget.js
+++ widget/tangy-acasi-widget.js
@@ -32,10 +32,10 @@ class TangyAcasiWidget extends TangyBaseWidget {
       ...config,
       ...element.getProps(),
       ...{
-        images: element.images,
-        touchsrc: element.touchsrc,
-        introsrc: element.introsrc,
-        transitionsrc: element.transitionsrc,
+        images: element.getAttribute('images'),
+        touchsrc: element.getAttribute('touchsrc'),
+        introsrc: element.hasAttribute('introsrc') ? element.getAttribute('introsrc'): '',
+        transitionsrc: element.getAttribute('transitionsrc'),
         tangyIf: element.hasAttribute('tangy-if')
           ? element.getAttribute('tangy-if').replace(/&quot;/g, '"')
           : '',
