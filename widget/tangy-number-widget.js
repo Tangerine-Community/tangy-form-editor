f2cc3ef71babdaab650eeb49793168c145fafb94
rjsteinert <rj@rjsteinert.com>
2019-08-20 05:59:34 -0400

    Implement attribute helpers and inner label on number widget

--- widget/tangy-number-widget.js
+++ widget/tangy-number-widget.js
@@ -10,52 +10,33 @@ class TangyNumberWidget extends TangyBaseWidget {
 
   get defaultConfig() {
     return {
-      name: '',
-      label: '',
-      hintText: '',
-      type: 'text',
-      required: false,
-      disabled: false,
-      hidden: false,
+      ...this.defaultConfigCommonAttributes(),
+      ...this.defaultConfigLabelAttributes(),
       allowedPattern: '',
       min: undefined,
       max: undefined,
-      tangyIf: '',
-      validIf: ''
+      innerLabel: ''
     };
   }
 
   upcast(config, element) {
-    // @TODO We have to do that final thing for tangyIf because it's not declared a prop in TangyInput.props thus won't get picked up by TangyInput.getProps().
     return {
-      ...config,
-      ...element.getProps(),
-      ...{
-        tangyIf: element.hasAttribute('tangy-if')
-          ? element.getAttribute('tangy-if').replace(/&quot;/g, '"')
-          : '',
-        validIf: element.hasAttribute('valid-if')
-          ? element.getAttribute('valid-if').replace(/&quot;/g, '"')
-          : ''
-      }
-    };
+      ...this.upcastCommonAttributes(config, element),
+      ...this.upcastLabelAttributes(config, element),
+      ...element.getProps()
+    }
   }
 
   downcast(config) {
     return `
       <tangy-input 
-        name="${config.name}"
-        label="${config.label}"
-        hint-text="${config.hintText}"
+        ${this.downcastCommonAttributes(config)}
+        ${this.downcastLabelAttributes(config)}
         type="number"
-        ${config.tangyIf === "" ? "" : `tangy-if="${config.tangyIf.replace(/"/g, '&quot;')}"`}
-        ${config.validIf === "" ? "" : `valid-if="${config.validIf.replace(/"/g, '&quot;')}"`}
+        inner-label="${config.innerLabel}"
         allowed-pattern="${config.allowedPattern}"
         ${config.min ? `min="${config.min}"` : ``}
         ${config.max ? `max="${config.max}"` : ``}
-        ${config.required ? 'required' : ''}
-        ${config.disabled ? 'disabled' : ''}
-        ${config.hidden ? 'hidden' : ''}
       ></tangy-input>
     `;
   }
@@ -65,9 +46,9 @@ class TangyNumberWidget extends TangyBaseWidget {
     const name = this.shadowRoot.querySelector('#name').innerHTML=`<span class="header-text">${config.name}</span>`
     return `${icon} ${name} ${this.downcast(config)}`;
   }
+
   renderPrint(config) {
     return `
-      
       <table>
       <tr><td><strong>Prompt:</strong></td><td>${config.label}</td></tr>
       <tr><td><strong>Variable Name:</strong></td><td>${config.name}</td></tr>
@@ -89,45 +70,18 @@ class TangyNumberWidget extends TangyBaseWidget {
       <hr/>
     `;
   }
+
   renderEdit(config) {
-    return `<h2>Add Number Input</h2>
+    return `
     <tangy-form id="tangy-number-widget">
       <tangy-form-item>
+        ${this.renderEditCommonAttributes(config)}
+        ${this.renderEditLabelAttributes(config)}
         <tangy-input
-          name="name" 
-          valid-if="input.value.match(/^[a-zA-Z].{1,}[a-zA-Z0-9\-_]$/)"
-          inner-label="Variable name"
-          hint-text="Enter any conditional validation logic. (e.g. input.value.length > 5)"
+          name="inner_label"
+          inner-label="Inner Label"
           value="${
-            config.name
-          }"
-          required>
-        </tangy-input>
-        <tangy-input 
-          name="label"
-          inner-label="Label"
-          hint-text="Enter the Question or Statement Text"
-          value="${
-            config.label
-          }">
-        </tangy-input>
-        <tangy-input
-          name="tangy_if"
-          label="Show if"
-          hint-text="Enter any conditional display logic. (e.g. getValue('isEmployee') === true)"
-          value="${config.tangyIf.replace(/"/g, '&quot;')}">
-        </tangy-input>
-        <tangy-input
-          name="valid_if"
-          label="Valid if"
-          hint-text="Enter any conditional validation logic. (e.g. input.value.length > 5)"
-          value="${config.validIf.replace(/"/g, '&quot;')}">
-        </tangy-input>
-        <tangy-input
-          name="hintText"
-          inner-label="Hint Text"
-          value="${
-            config.hintText
+            config.innerLabel
           }">
         </tangy-input>
         <tangy-input
@@ -156,15 +110,6 @@ class TangyNumberWidget extends TangyBaseWidget {
             config.max
           }">
         </tangy-input>
-        <tangy-checkbox name="required" ${
-          config.required ? 'value="on"' : ''
-        }>Required</tangy-checkbox>
-        <tangy-checkbox name="disabled" ${
-          config.disabled ? 'value="on"' : ''
-        }>Disabled</tangy-checkbox>
-        <tangy-checkbox name="hidden" ${
-          config.hidden ? 'value="on"' : ''
-        }>Hidden</tangy-checkbox>
       </tangy-form-item>
     </tangy-form>
     `;
@@ -173,40 +118,19 @@ class TangyNumberWidget extends TangyBaseWidget {
   onSubmit(config, formEl) {
     return {
       ...config,
-      name: formEl.response.items[0].inputs.find(input => input.name === 'name')
-        .value,
-      label: formEl.response.items[0].inputs.find(
-        input => input.name === 'label'
-      ).value,
+      ...this.onSubmitCommonAttributes(config, formEl),
+      ...this.onSubmitLabelAttributes(config, formEl),
       min: formEl.values.min,
       max: formEl.values.max,
-      hintText: formEl.values.hintText,
-      required:
-        formEl.response.items[0].inputs.find(input => input.name === 'required')
-          .value === 'on'
-          ? true
-          : false,
-      hidden:
-        formEl.response.items[0].inputs.find(input => input.name === 'hidden')
-          .value === 'on'
-          ? true
-          : false,
-      disabled:
-        formEl.response.items[0].inputs.find(input => input.name === 'disabled')
-          .value === 'on'
-          ? true
-          : false,
+      innerLabel: formEl.response.items[0].inputs.find(
+        input => input.name === 'inner_label'
+      ).value,
       allowedPattern: formEl.response.items[0].inputs.find(
         input => input.name === 'allowed_pattern'
-      ).value,
-      tangyIf: formEl.response.items[0].inputs.find(
-        input => input.name === 'tangy_if'
-      ).value,
-      validIf: formEl.response.items[0].inputs.find(
-        input => input.name === 'valid_if'
       ).value
-    };
+    }
   }
+
 }
 
 window.customElements.define('tangy-number-widget', TangyNumberWidget);
