0e38de7794cced7ba947ee8b41f6fe1537451061
rjsteinert <rj@rjsteinert.com>
2019-08-20 05:13:56 -0400

    Implement attribute helpers for date widget

--- widget/tangy-date-widget.js
+++ widget/tangy-date-widget.js
@@ -4,58 +4,39 @@ import 'tangy-form/input/tangy-select.js';
 import { TangyBaseWidget } from '../tangy-base-widget.js';
 
 class TangyDateWidget extends TangyBaseWidget {
+
   get claimElement() {
     return 'tangy-input[type=date]';
   }
 
   get defaultConfig() {
     return {
-      name: '',
-      label: '',
-      hintText: '',
       type: 'date',
-      required: false,
-      disabled: false,
-      hidden: false,
-      tangyIf: '',
-      validIf: ''
+      ...this.defaultConfigCommonAttributes(),
+      ...this.defaultConfigLabelAttributes()
     };
   }
 
   upcast(config, element) {
-    // @TODO We have to do that final thing for tangyIf because it's not declared a prop in TangyDate.props thus won't get picked up by TangyDate.getProps().
     return {
-      ...config,
-      ...element.getProps(),
-      ...{
-        tangyIf: element.hasAttribute('tangy-if')
-          ? element.getAttribute('tangy-if').replace(/&quot;/g, '"')
-          : '',
-        validIf: element.hasAttribute('valid-if')
-          ? element.getAttribute('valid-if').replace(/&quot;/g, '"')
-          : ''
-      }
+      ...this.upcastCommonAttributes(config, element),
+      ...this.upcastLabelAttributes(config, element),
+      ...element.getProps()
     };
   }
 
   downcast(config) {
     return `
       <tangy-input 
-        name="${config.name}"
-        label="${config.label}"
-        hint-text="${config.hintText}"
+        ${this.downcastCommonAttributes(config)}
+        ${this.downcastLabelAttributes(config)}
         type="date"
-        ${config.tangyIf === "" ? "" : `tangy-if="${config.tangyIf.replace(/"/g, '&quot;')}"`}
-        ${config.validIf === "" ? "" : `valid-if="${config.validIf.replace(/"/g, '&quot;')}"`}
-        ${config.required ? 'required' : ''}
-        ${config.disabled ? 'disabled' : ''}
-        ${config.hidden ? 'hidden' : ''}
       ></tangy-input>
     `;
   }
+
   renderPrint(config) {
     return `
-      
       <table>
         <tr><td><strong>Prompt:</strong></td><td>${config.label}</td></tr>
         <tr><td><strong>Variable Name:</strong></td><td>${config.name}</td></tr>
@@ -77,6 +58,7 @@ class TangyDateWidget extends TangyBaseWidget {
       <hr/>
     `;
   }
+
   renderInfo(config) {
     const icon = this.shadowRoot.querySelector('#icon').innerHTML=`<span class="header-text"><mwc-icon>date_range</mwc-icon><span>`
     const name = this.shadowRoot.querySelector('#name').innerHTML=`<span class="header-text">${config.name}</span>`
@@ -84,93 +66,23 @@ class TangyDateWidget extends TangyBaseWidget {
   }
 
   renderEdit(config) {
-    return `<h2>Add a Date Input</h2>
-    <tangy-form id="tangy-date-widget">
-      <tangy-form-item>
-        <tangy-input
-          name="name"
-          valid-if="input.value.match(/^[a-zA-Z].{1,}[a-zA-Z0-9\-_]$/)"
-          inner-label="Variable name" 
-          value="${
-            config.name
-          }"
-          hint-text="Enter the variable name that you would like displayed on all data outputs. Valid variable names start with a letter (a-z) with proceeding characters consisting of letters (a-z), underscore (_), dash (-), and numbers (0-9)."
-          required>
-        </tangy-input>
-        <tangy-input
-          name="label"
-          inner-label="Label"
-          hint-text="Enter the Question or Statement Text"
-          value="${
-            config.label
-          }">
-        </tangy-input>
-        <tangy-input 
-          name="hintText"
-          inner-label="Hint Text"
-          value="${
-            config.hintText
-          }">
-        </tangy-input>
-        <tangy-input
-          name="tangy_if"
-          inner-label="Show if"
-          hint-text="Enter any conditional display logic. (e.g. getValue('isEmployee') === true)"
-          value="${config.tangyIf.replace(/"/g, '&quot;')}">
-        </tangy-input>
-        <tangy-input
-          name="valid_if"
-          label="Valid if"
-          hint-text="Enter any conditional validation logic. (e.g. input.value.length > 5)"
-          value="${config.validIf.replace(/"/g, '&quot;')}">
-        </tangy-input>
-        <tangy-checkbox name="required" ${
-          config.required ? 'value="on"' : ''
-        }>Required</tangy-checkbox>
-        <tangy-checkbox name="disabled" ${
-          config.disabled ? 'value="on"' : ''
-        }>Disabled</tangy-checkbox>
-        <tangy-checkbox name="hidden" ${
-          config.hidden ? 'value="on"' : ''
-        }>Hidden</tangy-checkbox>
-      </tangy-form-item>
-    </tangy-form>
+    return `
+      <tangy-form id="tangy-date-widget">
+        <tangy-form-item>
+          ${this.renderEditCommonAttributes(config)}
+          ${this.renderEditLabelAttributes(config)}
+        </tangy-form-item>
+      </tangy-form>
     `;
   }
 
   onSubmit(config, formEl) {
     return {
-      ...config,
-      name: formEl.response.items[0].inputs.find(input => input.name === 'name')
-        .value,
-      label: formEl.response.items[0].inputs.find(
-        input => input.name === 'label'
-      ).value,
-      hintText: formEl.values.hintText,
-      required:
-        formEl.response.items[0].inputs.find(input => input.name === 'required')
-          .value === 'on'
-          ? true
-          : false,
-      hidden:
-        formEl.response.items[0].inputs.find(input => input.name === 'hidden')
-          .value === 'on'
-          ? true
-          : false,
-      disabled:
-        formEl.response.items[0].inputs.find(input => input.name === 'disabled')
-          .value === 'on'
-          ? true
-          : false,
-      tangyIf: formEl.response.items[0].inputs.find(
-        input => input.name === 'tangy_if'
-      ).value,
-      validIf: formEl.response.items[0].inputs.find(
-        input => input.name === 'valid_if'
-      ).value
-
+      ...this.onSubmitCommonAttributes(config, formEl),
+      ...this.onSubmitLabelAttributes(config, formEl)
     };
   }
+
 }
 
 window.customElements.define('tangy-date-widget', TangyDateWidget);
