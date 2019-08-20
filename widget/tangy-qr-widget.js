e14cb9e6e606e0390627a9f0a34724d1ff47d008
rjsteinert <rj@rjsteinert.com>
2019-08-20 06:18:41 -0400

    Add attribute helpers to qr widget

--- widget/tangy-qr-widget.js
+++ widget/tangy-qr-widget.js
@@ -5,54 +5,34 @@ import 'tangy-form/input/tangy-checkbox.js';
 import { TangyBaseWidget } from '../tangy-base-widget.js';
 
 class TangyQrWidget extends TangyBaseWidget {
+
   get claimElement() {
     return 'tangy-qr';
   }
 
   get defaultConfig() {
     return {
-      name: '',
-      required: false,
-      disabled: false,
-      hidden: false,
-      tangyIf: '',
-      validIf: ''
+      ...this.defaultConfigCommonAttributes()
     };
   }
 
   upcast(config, element) {
-    // @TODO We have to do that final thing for tangyIf because it's not declared a prop in TangyQr.props thus won't get picked up by TangyQr.getProps().
     return {
-      ...config,
-      ...element.getProps(),
-      ...{
-        tangyIf: element.hasAttribute('tangy-if')
-          ? element.getAttribute('tangy-if').replace(/&quot;/g, '"')
-          : '',
-        validIf: element.hasAttribute('valid-if')
-          ? element.getAttribute('valid-if').replace(/&quot;/g, '"')
-          : ''
-      }
+      ...this.upcastCommonAttributes(config, element)
     };
   }
 
   downcast(config) {
     return `
       <tangy-qr 
-        name="${config.name}"
-        ${config.tangyIf === "" ? "" : `tangy-if="${config.tangyIf.replace(/"/g, '&quot;')}"`}
-        ${config.validIf === "" ? "" : `valid-if="${config.validIf.replace(/"/g, '&quot;')}"`}
-        ${config.required ? 'required' : ''}
-        ${config.disabled ? 'disabled' : ''}
-        ${config.hidden ? 'hidden' : ''}
+        ${this.downcastCommonAttributes(config)}
       >
-        ${config.metaDataTemplate}
       </tangy-qr>
     `;
   }
+
   renderPrint(config) {
     return `
-
     <table>
       <tr><td><strong>Variable Name:</strong></td><td>${config.name}</td></tr>
       <tr><td><strong>Required:</strong></td><td>${config.required}</td></tr>
@@ -62,6 +42,7 @@ class TangyQrWidget extends TangyBaseWidget {
     <hr/>
     `;
   }
+
   renderInfo(config) {
     const icon = this.shadowRoot.querySelector('#icon').innerHTML=`<span class="header-text"><mwc-icon>filter_center_focus</mwc-icon><span>`
     const name = this.shadowRoot.querySelector('#name').innerHTML=`<span class="header-text">${config.name}</span>`
@@ -69,72 +50,21 @@ class TangyQrWidget extends TangyBaseWidget {
   }
 
   renderEdit(config) {
-    return `<h2>Add QR Scanner</h2>
-    <tangy-form id="tangy-qr">
-      <tangy-form-item>
-        <tangy-input
-          name="name"
-          valid-if="input.value.match(/^[a-zA-Z].{1,}[a-zA-Z0-9\-_]$/)"
-          inner-label="Variable name" 
-          value="${
-            config.name
-          }"
-          required>
-        </tangy-input>
-        <tangy-input 
-          name="tangy_if"
-          inner-label="Show if"
-          hint-text="Enter any conditional display logic. (e.g. getValue('isEmployee') === true)"
-          value="${config.tangyIf.replace(/"/g, '&quot;')}">
-        </tangy-input>
-        <tangy-input
-          name="valid_if"
-          inner-label="Valid if"
-          hint-text="Enter any conditional validation logic. (e.g. input.value.length > 5)"
-          value="${config.validIf.replace(/"/g, '&quot;')}">
-        </tangy-input>
-        <tangy-checkbox name="required" ${
-          config.required ? 'value="on"' : ''
-        }>Required</tangy-checkbox>
-        <tangy-checkbox name="disabled" ${
-          config.disabled ? 'value="on"' : ''
-        }>Disabled</tangy-checkbox>
-        <tangy-checkbox name="hidden" ${
-          config.hidden ? 'value="on"' : ''
-        }>Hidden</tangy-checkbox>
-      </tangy-form-item>
-    </tangy-form>
+    return `
+      <tangy-form id="tangy-qr">
+        <tangy-form-item>
+          ${this.renderEditCommonAttributes(config)}
+        </tangy-form-item>
+      </tangy-form>
     `;
   }
 
   onSubmit(config, formEl) {
     return {
-      ...config,
-      name: formEl.response.items[0].inputs.find(input => input.name === 'name')
-        .value,
-      required:
-        formEl.response.items[0].inputs.find(input => input.name === 'required')
-          .value === 'on'
-          ? true
-          : false,
-      hidden:
-        formEl.response.items[0].inputs.find(input => input.name === 'hidden')
-          .value === 'on'
-          ? true
-          : false,
-      disabled:
-        formEl.response.items[0].inputs.find(input => input.name === 'disabled')
-          .value === 'on'
-          ? true
-          : false,
-      tangyIf: formEl.response.items[0].inputs.find(
-        input => input.name === 'tangy_if'
-      ).value,
-      validIf: formEl.response.items[0].inputs.find(
-        input => input.name === 'valid_if'
-      ).value
-    };
+      ...this.onSubmitCommonAttributes(config, formEl),
+    }
   }
+
 }
 
 window.customElements.define('tangy-qr-widget', TangyQrWidget);
